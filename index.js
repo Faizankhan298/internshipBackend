e68aedf5db966f03cd7647893e34ee027196c73c
Mo Faizan <fk29837@gmail.com>
2024-07-21 23:52:08 +0530

    .

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const express =require("express")
-const app = express()
+const express = require("express");
+const app = express();
 require("dotenv").config();
 
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const router = require("./routes/userr")
-const mongoose = require("mongoose")
+const router = require("./routes/userr");
+const mongoose = require("mongoose");
 
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://internshipfrontend-po9g.onrender.com"],
+    // origin: ["https://internshipfrontend-po9g.onrender.com"],
+    origin: "*",
     credentials: true,
   })
 );
 
 const mongo_url = process.env.MONGO_CONN;
 
-
 mongoose.connect(mongo_url).then(() => {
   console.log("connected to database");
 });
 
-app.use("/auth",router)
+app.use("/auth", router);
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
-
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
