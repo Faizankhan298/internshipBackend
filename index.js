a01ab744f102469fcf035c218e8aa9ea2c895e2b
Mo Faizan <fk29837@gmail.com>
2024-07-21 19:43:56 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require("express")
 const app = express()
-require("dotenv").config();
-
+const env = require("dotenv");
+env.config();
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
